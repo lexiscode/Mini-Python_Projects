6430cc091b7ca71e49494360e67104c38001d222
lexiscode <nwokoriealex20@gmail.com>
2022-11-26 22:59:22 +0100

    Updated contents in Band-Name_Generator.py file

--- Band-Name_Generator.py
+++ Band-Name_Generator.py
@@ -1,3 +1,5 @@
+'''Write a program that helps a friend generate a cute music band name using their city and pet name.'''
+
 print("Welcome to the Band Name Generator.")
 
 city_name = input("What's the name of the city you grew up in?\n")
