ed5a7dfb9ffa4d3818309d824ba5602a94bde671
lexiscode <nwokoriealex20@gmail.com>
2022-11-28 08:50:53 +0100

    Updated content in Body-Mass_Index2.py file

--- Body-Mass_Index2.py
+++ Body-Mass_Index2.py
@@ -45,3 +45,19 @@ elif BMI > 30 and BMI < 35:
       print(f"Your BMI is {BMI}, you are obese.")
 else:
       print(f"Your BMI is {BMI}, you are clinically obese.")
+
+
+######################################################
+#Alternatively without needing the "and" operator
+#Result's interpretation
+if BMI < 18.5:
+        print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI < 25:
+        print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI < 30:
+        print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI < 35:
+        print(f"Your BMI is {BMI}, you are obese.")
+else:
+        print(f"Your BMI is {BMI}, you are clinically obese.")
+
