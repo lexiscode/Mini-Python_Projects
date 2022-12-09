3242ad452c23c6eb78bd4b02498fb70679ae0189
Nwokorie Alex V <nwokoriealex20@gmail.com>
2022-12-09 13:58:00 +0100

    Update Reeborgs_world.py

--- Reeborgs_world.py
+++ Reeborgs_world.py
@@ -116,7 +116,45 @@ while not at_goal():
         robot_jump()
 
 
+###############################################################################################
+#Hurdle 4
 
+'''
+Reeborg has entered a hurdle race. Make him run the course, following the path shown.
+
+The position, the height and the number of hurdles changes each time this world is reloaded.
+
+Your program should also be valid for worlds Hurdles 1, Hurdles 2 et Hurdles 3
+'''
+
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_around()
+    turn_left()
+
+    # has been modified below
+def robot_jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right() #else
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left() #else
+    
+#below remains unchanged regardless of which you use
+while not at_goal():
+    if wall_in_front():
+        robot_jump()
+    else:
+        move()
+        
 
 
 
