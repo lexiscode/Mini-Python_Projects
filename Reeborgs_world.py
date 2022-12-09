816b31ca680cabdd11944702c634536f62ea0e44
Nwokorie Alex V <nwokoriealex20@gmail.com>
2022-12-09 12:27:48 +0100

    Update Reeborgs_world.py

--- Reeborgs_world.py
+++ Reeborgs_world.py
@@ -19,5 +19,19 @@ def robot_jump():
     move()
     turn_left()
     
+#Below we can either use the For loop or the While loop
+#Using For loop
 for i in range(6): #range(0,6) and #range(0,6,1)
     robot_path()
+
+#Using While loop (1)
+number_of_hurdles = 1
+while number_of_hurdles <= 6:
+    robot_path()
+    number_of_hurdles += 1
+
+#Using While loop (2)
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    robot_path()
+    number_of_hurdles -= 1
