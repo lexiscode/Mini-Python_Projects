3082cef981937b6bbb1ce6171a8872c0505bdf0e
Nwokorie Alex V <nwokoriealex20@gmail.com>
2022-12-09 13:00:05 +0100

    Update Reeborgs_world.py

--- Reeborgs_world.py
+++ Reeborgs_world.py
@@ -1,6 +1,13 @@
 # THE HURDLE LOOP CHALLENGE
 #URL: http://bit.ly/3VKuZ3S
 
+'''
+Reeborg has entered a hurdles race. Make him run the course, following the path shown.
+What you need to know: The functions move() and turn_left().
+You may have noticed that your solution has some repeated patterns. If you know how to define functions, 
+define a function named robot_jump() and use it to simplify your program.
+'''
+
 def turn_around():
     turn_left()
     turn_left()
@@ -35,3 +42,66 @@ number_of_hurdles = 6
 while number_of_hurdles > 0:
     robot_path()
     number_of_hurdles -= 1
+    
+    
+############################################################################################
+
+
+'''
+Reeborg has entered a hurdle race, but he does not know in advance how long the race is. Make him run the course, 
+following a path similar to the one shown, but stopping at the only flag that will be shown after the race has started.
+
+What you need to know: The functions move() and turn_left().
+                       The condition at_goal() and its negation.
+                       How to use a while loop.
+Your program should also be valid for world Hurdles 1 above.
+
+The final required position of the robot will be chosen at random.
+'''
+while not at_goal():
+    robot_jump()
+    
+#OR Alternatively
+while at_goal() != True:
+    robot_jump()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
