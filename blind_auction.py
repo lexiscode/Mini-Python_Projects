34d5b67f30432b7337b53f9a139357b61cc26d59
Nwokorie Alex V <nwokoriealex20@gmail.com>
2022-12-14 16:35:16 +0100

    Update blind_auction.py

--- blind_auction.py
+++ blind_auction.py
@@ -2,9 +2,9 @@
 A first-price sealed-bid auction (FPSBA) is a common type of auction. It is also known as blind auction. 
 In this type of auction, all bidders simultaneously submit sealed bids so that no bidder knows the bid of any other participant. 
 The highest bidder pays the price that was submitted
-'''
+
 #####Example output
-'''
+
 What is your name?: Alex
 What is your bid?: $200
 Are there any other bidders? Type 'yes or 'no'.
